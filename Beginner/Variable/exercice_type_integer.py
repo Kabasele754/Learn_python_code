e44760c4513de164b8e02f623439900b45b9fc06
Kabasele <pepexykabasele@gmail.com>
2021-10-23 16:53:14 +0200

    exercice pour le type integer avec _ pour faire de traitement

--- Beginner/Variable/exercice_type_integer.py
+++ Beginner/Variable/exercice_type_integer.py
@@ -6,10 +6,36 @@ nbr2 = 4
 somme               = nbr1+nbr2
 multiplication      = nbr1*nbr2
 soustraction        = nbr1-nbr2
-division_entiere    = nbr1/nbr2
 division_floatante  = nbr1//nbr2
+division_entiere    = nbr1/nbr2
 modulo              = nbr1%nbr2
 puissance           = nbr1^nbr2
+
+print("La somme de deux nombres              :", nbr1, " + ", nbr2," = ", somme)
+print("La multication de deux nombres        :", nbr1, " + ", nbr2," = ", multiplication)
+print("La soustraction de deux nombres       :", nbr1, " + ", nbr2," = ", soustraction)
+print("La division entiere de deux nombres   :", nbr1, " + ", nbr2," = ", division_floatante)
+print("La division floatante de deux nombres :", nbr1, " + ", nbr2," = ", division_entiere)
+print("Le modulo de deux nombres             :", nbr1, " + ", nbr2," = ", modulo)
+print("La puissance de deux nombres          :", nbr1, " + ", nbr2," = ", puissance)
+
+print()
+
+
+# Une autre facon de faire ce de mettre _ sur le nombre
+
+print("Une autre facon de faire ce de mettre _ sur le nombre")
+print()
+
+nbr3 = 1_234_567_890
+nbr4 = 1_234_567_890
+somme               = nbr3+nbr4
+multiplication      = nbr3*nbr4
+soustraction        = nbr3-nbr4
+division_floatante  = nbr3/nbr4
+division_entiere    = nbr3//nbr4
+modulo              = nbr3%nbr4
+puissance           = nbr3^nbr4
 
 print("La somme de deux nombres              :", nbr1, " + ", nbr2," = ", somme)
 print("La multication de deux nombres        :", nbr1, " + ", nbr2," = ", multiplication)
@@ -18,7 +44,4 @@ print("La division floatante de deux nombres :", nbr1, " + ", nbr2," = ", divisi
 print("La division entiere de deux nombres   :", nbr1, " + ", nbr2," = ", division_floatante)
 print("Le modulo de deux nombres             :", nbr1, " + ", nbr2," = ", modulo)
 print("La puissance de deux nombres          :", nbr1, " + ", nbr2," = ", puissance)
-
-
-
 
