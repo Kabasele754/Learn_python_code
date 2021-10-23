edff2a1a84930016dc76a457a2e28c937e0b5fa6
Kabasele <pepexykabasele@gmail.com>
2021-10-23 16:43:04 +0200

    exercice pour le type integer ajout de commentaire

--- Beginner/Variable/exercice_type_integer.py
+++ Beginner/Variable/exercice_type_integer.py
@@ -3,6 +3,22 @@
 # Declaration de trois variables nbr1, nbr2 wt somme de type entier
 nbr1 = 5
 nbr2 = 4
-somme = nbr1+nbr2
+somme               = nbr1+nbr2
+multiplication      = nbr1*nbr2
+soustraction        = nbr1-nbr2
+division_entiere    = nbr1/nbr2
+division_floatante  = nbr1//nbr2
+modulo              = nbr1%nbr2
+puissance           = nbr1^nbr2
 
-print("La somme de trois nombres :", nbr1, " + ", nbr2," = ", somme)
+print("La somme de deux nombres              :", nbr1, " + ", nbr2," = ", somme)
+print("La multication de deux nombres        :", nbr1, " + ", nbr2," = ", multiplication)
+print("La soustraction de deux nombres       :", nbr1, " + ", nbr2," = ", soustraction)
+print("La division floatante de deux nombres :", nbr1, " + ", nbr2," = ", division_entiere)
+print("La division entiere de deux nombres   :", nbr1, " + ", nbr2," = ", division_floatante)
+print("Le modulo de deux nombres             :", nbr1, " + ", nbr2," = ", modulo)
+print("La puissance de deux nombres          :", nbr1, " + ", nbr2," = ", puissance)
+
+
+
+
