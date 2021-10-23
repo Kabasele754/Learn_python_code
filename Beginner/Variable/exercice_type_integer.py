a27f0a343429b142a8d763fe70ec178952e068d9
Kabasele <pepexykabasele@gmail.com>
2021-10-23 16:17:06 +0200

    exercice pour le type integer

---
+++ Beginner/Variable/exercice_type_integer.py
@@ -0,0 +1,8 @@
+# Somme de trois nombres avec l'operateur arithmetique +
+
+# Declaration de trois variables nbr1, nbr2 wt somme de type entier
+nbr1 = 5
+nbr2 = 4
+somme = nbr1+nbr2
+
+print("La somme de trois nombres :", nbr1, " + ", nbr2," = ", somme)
