ef5e3070de8e9ec168a2f51b8d796105032abc1f
Kabasele <pepexykabasele@gmail.com>
2021-10-24 05:01:35 +0200

    exemple pour le type string et modification type complex

--- Beginner/Variable/exemple_type_complex.py
+++ Beginner/Variable/exemple_type_complex.py
@@ -1,3 +1,12 @@
+"""
+Type complexe
+
+Ce complexsont les nombres qui viennent avec la partie réelle et imaginaire. 
+Un nombre complexe se présente sous la forme a+bj, 
+où a et b contiennent des nombres entiers ou des valeurs à virgule flottante.
+
+"""
+
 a = 3 + 5j
 print(a)  # (3+5j)
 print(type(a))  # <class 'complex'>
---
+++ Beginner/Variable/exemple_type_string.py
@@ -0,0 +1,8 @@
+"""
+Variable de chaîne
+
+
+En Python, une chaîne est un ensemble de caractères  représentés entre guillemets. 
+Python nous permet de définir une chaîne dans une paire de  guillemets simples  ou  doubles . 
+"""
+
