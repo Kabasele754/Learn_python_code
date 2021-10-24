47a9db3128c607f5b638bfea41b63b9140d42dc5
Kabasele <pepexykabasele@gmail.com>
2021-10-24 04:54:25 +0200

    exemple pour le type flaot

---
+++ Beginner/Variable/exemple_type_float.py
@@ -0,0 +1,11 @@
+"""
+Flotter
+En Python, les nombres à virgule flottante (float) sont des nombres réels positifs et négatifs 
+avec une partie fractionnaire indiquée par 
+le symbole décimal .ou la notation scientifique Eou e, par exemple 1234.56, 3.142, -1.55, 0.23.
+"""
+
+# create float variable
+salary = 10800.55
+print(salary)  # 10800.55
+print(type(salary))  # <class 'float'>
